45b4074ac63b6a276f0f8c8aed0acbfb186cffac
Chayakorn Jenpitak <chayakorn.jenpitak@jwt.com>
2017-07-11 15:45:07 +0700

    Update : Google Tag Manager

--- local/resources/views/layouts/inc/head.blade.php
+++ local/resources/views/layouts/inc/head.blade.php
@@ -16,6 +16,13 @@
     <!--[if lt IE 9]>
     <script type="text/javascript" src="{{ asset('/assets/js/core/html5shiv.js',env('URL_SSL',true)) }}"></script>
     <![endif]-->
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T3274JQ');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
 <script>
