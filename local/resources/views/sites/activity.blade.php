85d305e652835690362a847d1953e71a2cbfce61
chayakorn <bankj123@gmail.com>
2017-08-04 17:56:15 +0700

    no message

--- local/resources/views/sites/activity.blade.php
+++ local/resources/views/sites/activity.blade.php
@@ -39,7 +39,7 @@
                             <div class="col-10"></div>
                             <div class="col-80">
                                 <input maxlength="40" id="msgTxt" class="input-msg" type="text" value=""
-                                                       name="input-msg">
+                                       name="input-msg">
                             </div>
                             <div class="col-10"></div>
                         </div>
@@ -55,15 +55,15 @@
                         <div class="carousel-right"><img src="{{ asset('assets/img/ico_next-blue.png',env('URL_SSL',true)) }}"></div>
                         <div class="carousel-cont">
                             <ul class="carousel">
-                        <?php for ($i = 1; $i <= 6; $i++) { ?>
-                            <li>
-                                <a class="item" href="javascript:void(0);" rel="<?php echo $i; ?>">
-                                    <img src="{{ asset('assets/img/frame',env('URL_SSL',true)) }}/frame<?php echo $i; ?>.png" data-caption="">
-                                </a>
-                            </li>
-                        <?php } ?>
-                                </ul>
-                            </div>
+                                <?php for ($i = 1; $i <= 6; $i++) { ?>
+                                <li>
+                                    <a class="item" href="javascript:void(0);" rel="<?php echo $i; ?>">
+                                        <img src="{{ asset('assets/img/frame',env('URL_SSL',true)) }}/frame<?php echo $i; ?>.png" data-caption="">
+                                    </a>
+                                </li>
+                                <?php } ?>
+                            </ul>
+                        </div>
                     </div>
                     <div class="btn-edit-img">
                         <div class="box-btn">
@@ -96,7 +96,7 @@
                             <span class="img-share"><img src="{{ asset('assets/img/btn-share-fb.png',env('URL_SSL',true)) }}"
                                                          alt=""></span>
                         </a>
-                        <a class="btn-share btn-img-txt btn-line-share" href="#">
+                        <a id="social-line-share" class="btn-share btn-img-txt btn-line-share" href="#">
                             <span class="img-share"><img
                                         src="{{ asset('assets/img/btn-share-line.png',env('URL_SSL',true)) }}" alt=""></span>
                         </a>
@@ -104,16 +104,10 @@
                             <span class="img-share"><img src="{{ asset('assets/img/btn-share-tw.png',env('URL_SSL',true)) }}"
                                                          alt=""></span>
                         </a>
-
-                        <a  id="social-save" class="btn-share btn-img-txt btn-save" download="lovemomcard.gif" href="#" title="ImageName">
+                        <a  id="social-save" class="btn-share btn-img-txt btn-save" download="lovemomcard.gif" href="#" title="ImageName" onclick="Javascript:saveImage();">
                             <span class="img-share"><img
                                         src="{{ asset('assets/img/btn-share-save.png',env('URL_SSL',true)) }}" alt=""></span>
                         </a>
-
-                        {{--<a class="btn-share btn-img-txt btn-save" href="#">--}}
-                            {{--<span class="img-share"><img--}}
-                                        {{--src="{{ asset('assets/img/btn-share-save.png',env('URL_SSL',true)) }}" alt=""></span>--}}
-                        {{--</a>--}}
                     </div>
                     <div class="btn-group">
                         <div class="again-btn">
@@ -132,6 +126,11 @@
 @section('javascript')
 @section('javascript-content')
     <script>
+        window.mobileAndTabletcheck = function() {
+            var check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+            return check;
+        };
         var image = new Object();
         image.el = $('#image');
         image.crop;
@@ -173,6 +172,10 @@
             $('#import_image').on('change', function () {
                 readFile(this);
             });
+
+            if(window.mobileAndTabletcheck() == false){
+                $('#social-line-share').hide();
+            }
 
         });
         function fn_modifin_init() {
@@ -220,11 +223,8 @@
         }
 
         function fn_save_img() {
-
         }
-
         function fn_share_img() {
-
         }
 
         function crop_init() {
@@ -257,7 +257,7 @@
             var input = document.getElementById("msgTxt");
             $.ajax({
                 type: "POST",
-                url: "@if(env('APP_ENV') == 'prod'){{ secure_url('image/upload') }}@else{{ url('image/upload') }}@endif",
+                url: "{{ secure_url('image/upload') }}",
                 data: {
                     'image': dataURL,
                     "_token": token,
@@ -326,19 +326,16 @@
             window.open(url);
         }
 
-        function saveImage(b){
+        function saveImage(){
             ga('send','event','Save','Image');
-
-            window.open(canvas.toDataURL('image/gif'));
-            var gh = canvas.toDataURL('gif');
-
-            var a  = document.createElement('a');
-            a.href = b;
-            a.download = 'img.gif';
-
-            a.click()
-
-
+//            window.open(canvas.toDataURL('image/gif'));
+//            var gh = canvas.toDataURL('gif');
+//
+//            var a  = document.createElement('a');
+//            a.href = gh;
+//            a.download = b;
+//
+//            a.click()
             //var url = encodeURI(b);
             //window.open(url,'_blank');
         }
