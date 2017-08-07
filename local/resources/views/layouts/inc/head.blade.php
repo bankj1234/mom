b68528a22fb35901e9c3d547ab186060f874ab3a
chayakorn <bankj123@gmail.com>
2017-08-07 16:46:40 +0700

    new data production

--- local/resources/views/layouts/inc/head.blade.php
+++ local/resources/views/layouts/inc/head.blade.php
@@ -17,6 +17,11 @@
     <script type="text/javascript" src="{{ asset('/assets/js/core/html5shiv.js',env('URL_SSL',true)) }}"></script>
     <![endif]-->
     <style id="antiClickjack">body{display:none !important;}</style>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T3274JQ');</script>
     <script type="text/javascript">
         if (self === top) {
             var antiClickjack = document.getElementById("antiClickjack");
@@ -56,7 +61,6 @@
         a.src = g;
         m.parentNode.insertBefore(a, m)
     })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
     ga('create', 'UA-33354168-14', 'auto');
     ga('send', 'pageview');
 </script>
